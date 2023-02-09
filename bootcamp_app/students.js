8891c8641df354780c8825e214816446a8bdd4f8
Erik Anderson <108945295+ErikAndersonWebDev@users.noreply.github.com>
2023-02-08 18:25:16 -0800

    Update student and teacher files to use parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -6,13 +6,14 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-
+const cohort = process.argv[2];
+const limit = process.argv[3];
 pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
+WHERE cohorts.name LIKE '%${cohort}%'
+LIMIT ${limit};
 `)
 .then(res => {
   res.rows.forEach(user => {
